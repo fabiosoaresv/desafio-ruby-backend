c0f779a336c8ce9b042d59bee02fb6d5ff8c8668
Fabio Soares Venturelli <fabio.venturelli@meta.com.br>
2021-02-23 17:14:50 -0300

    Add logic API Importer and call Service

--- backend/app/controllers/api/v1/importer_controller.rb
+++ backend/app/controllers/api/v1/importer_controller.rb
@@ -1,2 +1,33 @@
-class ImporterController < ActionController::Base
+# frozen_string_literal: true
+
+module Api::V1
+  class ImporterController < ActionController::Base
+    skip_before_action :verify_authenticity_token
+    before_action :importer_params
+
+    def create
+      key_file = params.keys.reject { |x| ['action', 'controller'].include?(x) }&.first
+      path = params[key_file]&.path
+
+      return default_error unless path.present?
+
+      importer = Importer.new(path)
+
+      if importer.import_and_create_data
+        render json: { success: true, message: 'Upload and creation data make with successful!' }
+      else
+        default_error
+      end
+    end
+
+    private
+
+    def default_error
+      render json: { success: false, message: 'Something is wrong!' }
+    end
+
+    def importer_params
+      params.permit(:id)
+    end
+  end
 end
