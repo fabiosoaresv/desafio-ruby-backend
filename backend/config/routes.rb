5e78a3a14de2430f9af6265746b81a2864e7e518
Fabio Soares Venturelli <fabio.venturelli@meta.com.br>
2021-02-24 18:44:11 -0300

    Add endpoint GET all transactions

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :importer, only: [:index] do
         collection { post :import_file, via: :options }
       end
+      resources :transactions, only: [:index]
     end
   end
 end
